6fd0ba3650b3548b6c9ceeadcc33fb266236f636
Tomasz Orda <tomaszorda@outlook.com>
2025-12-05 12:11:42 +0100

    Add commented test function blank to the template

--- template_skeleton.rs
+++ template_skeleton.rs
@@ -3,3 +3,10 @@ pub fn solution(input: String) -> String {
  
     format!("{:?}",input) 
 } 
+ 
+// #[test] 
+// fn basic_test() { 
+//     let input:String = ""
+//                          .chars().filter(|&c| c!=' ').collect::<String>();
+//     assert_eq!(solution(input), "".to_string()) 
+// } 
